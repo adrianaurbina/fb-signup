cb2f9722832e8c9a7b54be5877bdf25e05a73f27
Erick Jiménez <erick9025@hotmail.com>
2025-06-01 12:31:41 -0600

    Erick final needs clean

---
+++ src/test/java/Models/PersonModel.java
@@ -0,0 +1,15 @@
+package test.java.Models;
+
+import java.util.Date;
+
+import Utils.EnumGender;
+
+public class PersonModel {
+	public String firstname;
+	public String lastname;
+	public String mailorphone;
+	public String password;
+	public Date birthdate;
+	public EnumGender gender;
+	
+}
